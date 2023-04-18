30649b1fb0e4c53f0eb893ed0ca896fe1a40de76
Olivia Moreau <moreauolivia49@gmail.com>
2023-04-18 14:58:53 +0200

    feat: initialize chakra ui and routes

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+import './index.css';
 
+export const theme = extendBaseTheme({
+  colors: {
+    purple: '#B2A4FF',
+    yellow: '#FFC984',
+    pink: '#FFA1A1',
+    green: '#A4FFCE',
+    black: '#454545',
+    whitePurple: '#F7F6FF',
+    white: '#FFFFFF',
+  },
+  fonts: {
+    heading: `'gothic-a1', sans-serif`,
+    body: `'gothic-a1', sans-serif`,
+  },
+});
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraBaseProvider theme={theme}>
+      <App />
+    </ChakraBaseProvider>
+  </React.StrictMode>
+);
---
+++ front/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Heading } from '@chakra-ui/react';
+
+const Home = () => {
+  return (
+    <Heading color={'purple'} fontSize={'100'}>
+      Home
+    </Heading>
+  );
+};
+
+export default Home;
