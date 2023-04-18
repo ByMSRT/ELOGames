d92621970712e152deef9b4c245f07f3685c1720
Olivia Moreau <moreauolivia49@gmail.com>
2023-04-18 14:58:53 +0200

    feat: initialize chakra ui and routes

---
+++ front/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { Heading } from '@chakra-ui/react';
+import React from 'react';
+const Login = () => {
+  return (
+    <Heading color={'purple'} fontSize={'100'}>
+      Login
+    </Heading>
+  );
+};
+
+export default Login;
