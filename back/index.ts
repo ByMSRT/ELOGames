567f11446ac0164c96ee3e284c7632d02935766b
Elouan <73824665+ByMSRT@users.noreply.github.com>
2023-05-02 15:15:38 +0200

    Add cors policy

--- back/index.ts
+++ back/index.ts
@@ -4,10 +4,13 @@ import { userRouter } from "./src/components/Client/userRouter";
 import { clientRouter } from "./src/components/Client/clientRouter";
 import { invoicesRouter } from "./src/components/Invoices/invoicesRouter";
 import { gameRouter } from "./src/components/Game/gameRouter";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/user", userRouter);
 app.use("/client", clientRouter);
