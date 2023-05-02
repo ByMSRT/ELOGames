93a270d818d7e0250f1f66c4b4df365a5225517a
Louis <louis.teilliais@gmail.com>
2023-05-02 10:44:13 +0200

    login & register page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Register from './pages/Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
 
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
