f3d32f77852ddd3a283d36d8743e3cd82692ceeb
Olivia Moreau <moreauolivia49@gmail.com>
2023-05-02 17:04:48 +0200

    feat: pagination and itemsPerPage filter

--- front/src/components/pages/AdaptativeTab.tsx
+++ front/src/components/pages/AdaptativeTab.tsx
@@ -9,6 +9,9 @@ import {
   Button,
   Box,
   Td,
+  InputGroup,
+  InputRightElement,
+  Select,
 } from '@chakra-ui/react';
 import { TabBuilder } from '../../utils/types';
 import CustomTd from './CustomTd';
@@ -21,12 +24,20 @@ import { billsData, clientsData, gamesData } from '../../utils/tempData';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { MaterialSymbol } from 'react-material-symbols';
+import Pagination from '../shared/Pagination';
 
 const AdaptativeTab = ({ type }: { type: 'clients' | 'bills' | 'games' }) => {
   const location = useLocation();
   const [search, setSearch] = useState('');
   const [data, setData] = useState<any[]>([]);
   const [filteredData, setFilteredData] = useState<any[]>([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(5);
+
+  const indexOfLastItem = currentPage * postsPerPage;
+  const indexOfFirstItem = indexOfLastItem - postsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
 
   const [columns, setColumns] = useState<TabBuilder[]>([]);
 
@@ -52,6 +63,7 @@ const AdaptativeTab = ({ type }: { type: 'clients' | 'bills' | 'games' }) => {
   }, [typeUrl]);
 
   const searching = () => {
+    setCurrentPage(1);
     let tempData = data.filter((obj) => {
       return Object.keys(obj).some((key) => {
         const value = obj[key].toString().toLowerCase();
@@ -61,50 +73,97 @@ const AdaptativeTab = ({ type }: { type: 'clients' | 'bills' | 'games' }) => {
     setFilteredData(tempData);
   };
 
+  const removeSearching = () => {
+    setCurrentPage(1);
+    setSearch('');
+    setFilteredData(data);
+  };
+
   return (
-    <>
+    <Box
+      height="100%"
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'space-between'}
+      p={5}
+    >
       <Box display="flex" width="xl">
-        <Input value={search} onChange={(e) => setSearch(e.target.value)} />
+        <InputGroup>
+          <Input value={search} onChange={(e) => setSearch(e.target.value)} />
+          <InputRightElement
+            children={
+              <Button variant="ghost" onClick={removeSearching}>
+                <MaterialSymbol icon="close" />
+              </Button>
+            }
+          />
+        </InputGroup>
+
         <Button onClick={searching}>
-          <MaterialSymbol icon="loupe"></MaterialSymbol>
+          <MaterialSymbol icon="search"></MaterialSymbol>
         </Button>
+        <Select
+          width={'xs'}
+          value={postsPerPage}
+          onChange={(e) => {
+            setPostsPerPage(parseInt(e.target.value));
+            setCurrentPage(1);
+          }}
+        >
+          <option defaultValue={5} value={5}>
+            5
+          </option>
+          <option value={10}>10</option>
+          <option value={20}>20</option>
+          <option value={30}>30</option>
+        </Select>
       </Box>
-      <TableContainer>
-        <Table>
-          <Thead>
-            <Tr>
-              {columns! &&
-                columns!.map((column, index) => {
-                  return <Th key={index}>{column.name}</Th>;
+      <Box overflowX="scroll" height="100%">
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                {columns! &&
+                  columns!.map((column, index) => {
+                    return <Th key={index}>{column.name}</Th>;
+                  })}
+                <Th>Éditer</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {currentItems &&
+                currentItems.map((item, index) => {
+                  return (
+                    <Tr key={index}>
+                      {columns &&
+                        columns.map((column, index) => {
+                          return (
+                            <CustomTd
+                              key={index}
+                              type={column.type}
+                              value={item[column.dbColumn]}
+                            ></CustomTd>
+                          );
+                        })}
+                      <Td>
+                        <Button>Éditer</Button>
+                      </Td>
+                    </Tr>
+                  );
                 })}
-              <Th>Éditer</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {filteredData &&
-              filteredData.map((item, index) => {
-                return (
-                  <Tr key={index}>
-                    {columns &&
-                      columns.map((column, index) => {
-                        return (
-                          <CustomTd
-                            key={index}
-                            type={column.type}
-                            value={item[column.dbColumn]}
-                          ></CustomTd>
-                        );
-                      })}
-                    <Td>
-                      <Button>Éditer</Button>
-                    </Td>
-                  </Tr>
-                );
-              })}
-          </Tbody>
-        </Table>
-      </TableContainer>
-    </>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+      <Pagination
+        currentPage={currentPage}
+        itemsPerPage={postsPerPage}
+        onChangePage={(page: number) => {
+          setCurrentPage(page);
+        }}
+        totalItems={filteredData.length}
+      ></Pagination>
+    </Box>
   );
 };
 
