04b45fe43d34ad4a8d2b88f25ef1ea8440016e6d
Elouan <73824665+ByMSRT@users.noreply.github.com>
2023-05-02 15:06:53 +0200

    Add last routes

--- back/index.ts
+++ back/index.ts
@@ -1,12 +1,18 @@
 import "dotenv/config";
 import express from "express";
 import { userRouter } from "./src/components/Client/userRouter";
+import { clientRouter } from "./src/components/Client/clientRouter";
+import { invoicesRouter } from "./src/components/Invoices/invoicesRouter";
+import { gameRouter } from "./src/components/Game/gameRouter";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/user", userRouter);
+app.use("/client", clientRouter);
+app.use("/invoice", invoicesRouter);
+app.use("/game", gameRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ` + process.env.PORT);
