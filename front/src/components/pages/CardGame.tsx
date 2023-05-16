304ff408c00cc5754189dea46f6971e2a676331e
Louis <louis.teilliais@gmail.com>
2023-05-16 10:26:20 +0200

    feat: try adding store

--- front/src/components/pages/CardGame.tsx
+++ front/src/components/pages/CardGame.tsx
@@ -1,86 +1,67 @@
-
-import { Box , Card, CardBody, CardFooter, Divider, Heading, Image, Text, Highlight } from '@chakra-ui/react';
-
-import alarm from '../../assets/alarm.svg'
-import group from '../../assets/diversity.svg'
-import shopping_cart from '../../assets/add_shopping_cart.svg'
-import photo from '../../assets/aventuriers-du-rail-europe-15-ans 1.png'
-
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Stack,
+  Image,
+  Text,
+  Highlight,
+} from '@chakra-ui/react';
+import alarm from '../../assets/alarm.svg';
+import group from '../../assets/diversity.svg';
+import shopping_cart from '../../assets/add_shopping_cart.svg';
+import photo from '../../assets/aventuriers-du-rail-europe-15-ans 1.png';
 
 const CardGame = () => {
-
-    return (
-        <Card minW='xs' bg="#F7F6FF">
-          <CardBody
-            display='flex'
-            justifyContent='center'
+  return (
+    <Card minW="xs" bg="#F7F6FF">
+      <CardBody display="flex" justifyContent="center">
+        <Image w="13rem" h="13rem" src={photo} />
+      </CardBody>
+      <Divider />
+      <CardFooter h="9rem" justifyContent="space-between">
+        <Box>
+          <Highlight
+            query="Disponible"
+            styles={{
+              px: '1',
+              py: '1',
+              bg: 'green',
+              color: '#138A2D',
+              rounded: 'full',
+            }}
           >
-            <Image
-              w='13rem'
-              h='13rem'
-              src={photo}
-            />
-          </CardBody>
-          <Divider />
-          <CardFooter 
-            h='9rem'
-            justifyContent='space-between'
-          >
-          <Box>
-            <Highlight query='Disponible' styles={{ px: '1', py: '1', bg: 'green', color: '#138A2D', rounded: 'full' }}>
-              Disponible
-            </Highlight>
-            <Heading
-              size='md'
-              mt='2'
-            >
-              Game 1
-            </Heading>
-            <Text
-              fontSize='sm'
-            >
-              Jeu de plateau
-            </Text>
+            Disponible
+          </Highlight>
+          <Heading size="md" mt="2">
+            Game 1
+          </Heading>
+          <Text fontSize="sm">Jeu de plateau</Text>
 
-            <Box
-              display='flex'
-              mt='0.5'
-              gap='2'
-            >
-              <Image
-                src={alarm}
-              />
-              <Text>
-                25-30min
-              </Text>
+          <Box display="flex" mt="0.5" gap="2">
+            <Image src={alarm} />
+            <Text>25-30min</Text>
 
-              <Image
-                src={group}
-              />
-              <Text>
-                2-7 
-              </Text>
-            </Box>
-
+            <Image src={group} />
+            <Text>2-7</Text>
           </Box>
-          <Box
-            display='flex'
-            flexDirection='column'
-            mt='0.5'
-            gap='10'
-            justifyContent='space-between'
-          >
-            <Image
-              src={shopping_cart}
-            />
-            <Text>
-              35.5€
-            </Text>
-          </Box>
-        </CardFooter>
-      </Card>
-    )
-
-}
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          mt="0.5"
+          gap="10"
+          justifyContent="space-between"
+        >
+          <Image src={shopping_cart} />
+          <Text>35.5€</Text>
+        </Box>
+      </CardFooter>
+    </Card>
+  );
+};
 
 export default CardGame;
--- front/src/pages/Shop.tsx
+++ front/src/pages/Shop.tsx
@@ -1,64 +1,66 @@
-import { Box, Button, Grid, Input, InputGroup, InputRightElement, Select } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Grid,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Select,
+} from '@chakra-ui/react';
 import NavbarShop from '../components/shared/NavbarShop';
 import CardGame from '../components/pages/CardGame';
 import { MaterialSymbol } from 'react-material-symbols';
 
 const Shop = () => {
+  return (
+    <Box display="flex" alignItems="center" flexDirection="column">
+      <NavbarShop />
+      <Box mt="1rem" w="40rem">
+        <InputGroup size="md">
+          <Input placeholder="Rechercher..." />
+          <InputRightElement width="4.5rem">
+            <Button background="white" h="1.75rem" size="sm">
+              <MaterialSymbol icon="search"></MaterialSymbol>
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Grid templateColumns="repeat(3, 1fr)" gap={5} mt="1rem">
+          <Select placeholder="Catégories">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+          <Select placeholder="Nb joueurs">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+          <Select placeholder="Age">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+        </Grid>
+      </Box>
 
-    return (
-        <Box display="flex" alignItems="center" flexDirection="column">
-            <NavbarShop />
-            <Box mt="1rem" w="40rem">
-                <InputGroup size="md">
-                    <Input placeholder="Rechercher..." />
-                    <InputRightElement width="4.5rem">
-                        <Button background="white" h="1.75rem" size="sm">
-                            <MaterialSymbol icon="search"></MaterialSymbol>
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-                <Grid templateColumns="repeat(3, 1fr)" gap={5} mt="1rem">
-                    <Select placeholder="Catégories">
-                        <option value="option1">Option 1</option>
-                        <option value="option2">Option 2</option>
-                        <option value="option3">Option 3</option>
-                    </Select>
-                    <Select placeholder="Nb joueurs">
-                        <option value="option1">Option 1</option>
-                        <option value="option2">Option 2</option>
-                        <option value="option3">Option 3</option>
-                    </Select>
-                    <Select placeholder="Age">
-                        <option value="option1">Option 1</option>
-                        <option value="option2">Option 2</option>
-                        <option value="option3">Option 3</option>
-                    </Select>
-                </Grid>
-            </Box>
-
-            <Box
-                display="flex"
-                alignItems="center"
-                flexDirection="column"
-                mt="10"
-            >
-                <Grid templateColumns="repeat(3, 1fr)" gap={20}>
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                    <CardGame />
-                </Grid>
-            </Box>
-        </Box>
-    );
+      <Box display="flex" alignItems="center" flexDirection="column" mt="10">
+        <Grid templateColumns="repeat(3, 1fr)" gap={20}>
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+          <CardGame />
+        </Grid>
+      </Box>
+    </Box>
+  );
 };
 
 export default Shop;
